8967fe77420e1b209ef48c65e2b34289471baa89
ivyrose <iliana.vargas@northwestern.edu>
2019-11-16 17:04:40 -0600

    Delete CSS and add javascript
    clear.
    clear

--- questions.js
+++ questions.js
@@ -29,10 +29,39 @@ var questions = [
         choices: ["The Articles of Confederation", "The Declaration of Indeendence", "The Emancipation Proclamations", "Magna Carta"],
         answer: "The Articles of Confederation"
     }
-]
+];
+
+function showquestions () { 
+        // This is the container that will contain all the buttons
+    // var buttonContain = document.getElementById("buttons");
+    var buttonContain = $("#buttons");
+
+    var titleDiv = $("#title").text(questions[0].title);
+
+    // For every choice make a button and then append it to the container
+    for (var i = 0; i < questions[0].choices.length; i++) {
+
+        // For each drink in the array, we create a new paragraph to hold that text.
+        // A new paragraph will be created with each iteration of the loop.
+        // var button = document.createElement("button");
+        var button = $("<button>")
+
+        // We then assign the the text of this paragraph to be the text in the array.
+        button.text(questions[0].choices[i])
+
+        // We then add the paragraph to the our main div on the page ("#drink-options")
+        buttonContain.append(button);
+    }
+}
+
+showquestions()
 
 /**
+ * Break this down into steps
  * show the title, generate five buttons
  * click listener, tell when buttons are clicked, check if answer is correct or not, afterwards show next question
  * If all the questions are done, show the score
+ * maybe for each question
  */
+
+ 
