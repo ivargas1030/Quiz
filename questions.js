5b5cafeb869fe2fe1dff3e58cdcaa441c8d2f53a
ivyrose <iliana.vargas@northwestern.edu>
2019-11-15 21:54:14 -0600

    Questions added

---
+++ questions.js
@@ -0,0 +1,32 @@
+var questions = [
+    {
+        title: "The Constitutional Convention met in -",
+        choices: ["New York", "Philadelphia", "Washington D.C.", "Boston"],
+        answer: "Philadelphia"
+    },
+
+    {
+        title: "The number of main branches in our Government Is:",
+        choices: ["6", "21", "7", "3"],
+        answer: "3"
+    },
+
+    {
+        title: "The Legislative Branch of our goverment is called:",
+        choices: ["The Cabinet", "Congress", "Supreme Court", "The Presidency"],
+        answer: "Congress"
+    },
+
+    {
+        title: "The number of amendments that were originally added to the COnsitution in 1791 is:",
+        choices: ["3", "1", "10", "13"],
+        answer: "10"
+
+    },
+
+    {
+        title: "The Unites States first Constitution was called:",
+        choices: ["The Articles of Confederation", "The Declaration of Indeendence", "The Emancipation Proclamations", "Magna Carta"],
+        answer: "The Articles of Confederation"
+    }
+]
